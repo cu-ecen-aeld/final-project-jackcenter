7c4d0ffb30b571a566daba1324e99eae4f6cd74a
Jack <jack.d.center@gmail.com>
2025-04-18 14:04:23 -0600

    Load PWM at Startup

--- base_external/package/servo-driver/servo-driver.mk
+++ base_external/package/servo-driver/servo-driver.mk
@@ -1,4 +1,4 @@
-SERVO_DRIVER_VERSION = 6810ce33451e6f54540175339f527d28db19e292
+SERVO_DRIVER_VERSION = fd165d7e0685f04187080651042f17a0b692cc59
 SERVO_DRIVER_SITE = git@github.com:jackcenter/final-project-application-servo-driver.git
 SERVO_DRIVER_SITE_METHOD = git
 SERVO_DRIVER_GIT_SUBMODULES = NO
@@ -7,6 +7,7 @@ SERVO_DRIVER_MODULE_SUBDIRS = servo_driver_module
 
 define SERVO_DRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/servo_driver_module/scripts/servo_driver_init.sh $(TARGET_DIR)/etc/init.d/S99servodriver
+	$(INSTALL) -m 0755 $(@D)/servo_driver_module/scripts/pwm_init.sh $(TARGET_DIR)/etc/init.d/S98pwminit
 endef
 
 $(eval $(kernel-module))
