f9af07c1323dcdc43d58198f1fe28f285ce44bff
Jack <jack.d.center@gmail.com>
2025-04-28 14:35:22 -0600

    Add Second Servo Driver Device (ASD-6)

--- base_external/package/servo-driver/servo-driver.mk
+++ base_external/package/servo-driver/servo-driver.mk
@@ -1,4 +1,4 @@
-SERVO_DRIVER_VERSION = 6aa50200f6247b4a7f58da278abab24b971734df
+SERVO_DRIVER_VERSION = 5d970e6e1931790a57a82b6ccd7a811aaf748eea
 SERVO_DRIVER_SITE = git@github.com:jackcenter/final-project-application-servo-driver.git
 SERVO_DRIVER_SITE_METHOD = git
 SERVO_DRIVER_GIT_SUBMODULES = NO
@@ -12,7 +12,6 @@ endef
 define SERVO_DRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/servo_driver/bin/servo_driver $(TARGET_DIR)/usr/bin/servo_driver
 	$(INSTALL) -m 0755 $(@D)/servo_driver_module/scripts/servo_driver_init.sh $(TARGET_DIR)/etc/init.d/S99servodriver
-	$(INSTALL) -m 0755 $(@D)/servo_driver_module/scripts/pwm_init.sh $(TARGET_DIR)/etc/init.d/S98pwminit
 endef
 
 $(eval $(kernel-module))
